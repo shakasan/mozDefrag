913708329ed0870e1b1fc12e76a29753f124ac54
shakasan <shakasan@sirenacorp.be>
2017-09-30 12:11:45 +0200

    cx_Freeze build setup file added

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+mozDefrag cx_Freeze setup file
+"""
+
+from cx_Freeze import setup, Executable
+
+
+setup(
+    name="mozDefrag",
+    version="0.1",
+    description="defrag Firefox/Thunderbird's sqlite DB for users/profiles",
+    executables=[Executable("mozDefrag.py")],
+)
